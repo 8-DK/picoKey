8c488f5eb768f4a27ee5b02d23ff9aecd7b921a1
8-DK <khairnardm@gmail.com>
2022-01-15 00:44:32 +0530

    fix usb with I2c lib

--- src/displayHelper.cpp
+++ src/displayHelper.cpp
@@ -56,7 +56,7 @@ void DisplayHelper::displayLoop()
             myOled.write_string(0, 0, 5, (char *)"Written by L BANK", FONT_8x8, 0, 1);
             myOled.write_string(0, 4, 6, (char *)"Pico by M KOOIJ", FONT_8x8, 0, 1);
             myOled.write_string(0, 0, 7, (char *)"**************** ", FONT_8x8, 0, 1);
-            sleep_ms(5000); //vTaskDelay(5000 / portTICK_PERIOD_MS);
+            vTaskDelay(5000 / portTICK_PERIOD_MS);
         }
 
         myOled.fill(0, 1);
@@ -65,13 +65,13 @@ void DisplayHelper::displayLoop()
         myOled.write_string(0, 0, 2, (char *)"16x32 and a new", FONT_8x8, 0, 1);
         myOled.write_string(0, 0, 3, (char *)"Stretched", FONT_12x16, 0, 1);
         myOled.write_string(0, 0, 5, (char *)"from 6x8", FONT_12x16, 0, 1);
-        sleep_ms(5000);//vTaskDelay(5000 / portTICK_PERIOD_MS);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
 
         int x, y;
         myOled.fill(0, 1);
         myOled.write_string(0, 0, 0, (char *)"Backbuffer Test", FONT_NORMAL, 0, 1);
         myOled.write_string(0, 0, 1, (char *)"96 lines", FONT_NORMAL, 0, 1);
-       sleep_ms(2000);// vTaskDelay(2000 / portTICK_PERIOD_MS);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
         for (x = 0; x < OLED_WIDTH - 1; x += 2)
         {
             myOled.draw_line(x, 0, OLED_WIDTH - x, OLED_HEIGHT - 1, 1);
@@ -81,7 +81,7 @@ void DisplayHelper::displayLoop()
             myOled.draw_line(OLED_WIDTH - 1, y, 0, OLED_HEIGHT - 1 - y, 1);
         };
         myOled.write_string(0, 0, 1, (char *)"Without backbuffer", FONT_SMALL, 0, 1);
-        sleep_ms(2000);//vTaskDelay(2000 / portTICK_PERIOD_MS);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
         myOled.fill(0, 1);
         for (x = 0; x < OLED_WIDTH - 1; x += 2)
         {
@@ -93,6 +93,9 @@ void DisplayHelper::displayLoop()
         }
         myOled.dump_buffer(ucBuffer);
         myOled.write_string(0, 0, 1, (char *)"With backbuffer", FONT_SMALL, 0, 1);
-        sleep_ms(2000);//vTaskDelay(2000 / portTICK_PERIOD_MS);
+        while (1)
+        {       
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
+        }
     }
 }
